4f27089bf9d0d064ceb1008e5a3fa15a5eab4e83
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2024-02-15 20:22:17 -0300

    função de fetch

--- src/components/buy_now/BuyNow.tsx
+++ src/components/buy_now/BuyNow.tsx
@@ -1,11 +1,43 @@
 import Header from "../buy_now/Header";
+import React, { useState, useEffect } from 'react';
+import ItemCard from "./ItemCard";
+
 
 function BuyNow() {
+    const [itens, setItens] = useState([]);
+
+    useEffect(() => {
+        async function fetchItens() {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+            
+                if (!response.ok) {
+                  throw new Error(`Erro de Rede: ${response.status}`);
+                }
+            
+                const jsonData =  await response.json();
+                setItens(jsonData);
+                
+                
+              } catch (error) {
+                console.error('Erro ao buscar os dados:', error);
+              }
+        }
+    
+        fetchItens();
+    }, []);
+
     return (
         <div>
             <Header content={'Itens para compra:'} />
 
-
+            <div id="div-list">
+                {
+                    itens.map(item => (
+                        <ItemCard thumbnailUrl={item["thumbnailUrl"]} title={item["title"]} price={item["id"]} url={item["url"]}/>
+                    ))
+                }
+            </div>
         </div>
     );
 }
