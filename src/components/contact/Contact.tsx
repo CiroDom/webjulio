568bebec6859762c7e3d576a3e08cb5e619fe401
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2024-02-26 17:04:35 -0300

    adição dos hooks

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,6 +1,12 @@
 import Header from '../buy_now/Header';
+import React, { useState, useEffect } from 'react';
 
 function Contact() {
+    const [name, setName] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+
     return (
         <div className={'container'}>
             <Header content={'Itens para compra:'} />
@@ -8,13 +14,34 @@ function Contact() {
             <div className={'form-container'}>
                 <form>
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text" name="name" placeholder="Your name" required />
+                    <input 
+                        id="name"
+                        type="text"
+                        name="name"
+                        placeholder="Your name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        required />
     
                     <label htmlFor="subject">Subject</label>
-                    <input id="subject" type="text" name="subject" placeholder="Subject" required />
+                    <input
+                        id="subject"
+                        type="text"
+                        name="subject"
+                        placeholder="Subject"
+                        onChange={(e) => setSubject(e.target.value)}
+                        value={subject}
+                        required />
     
                     <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" placeholder="Write something.." style={{ height: '570px' }} required></textarea>
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder="Write something.."
+                        style={{height: '570px'}}
+                        onChange={(e) => setMessage(e.target.value)}
+                        value={message}
+                        required></textarea>
 
                     <input id="inputButton" type="submit" value="Submit" />
                 </form>
